774d8a1a1c7d62467d5aa70f49fe686d67ff2126
akarpushev <akarpushev@yandex.ru>
2024-04-18 21:12:46 +0300

    18.04.24 практика 5 начата

--- apps/api/views.py
+++ apps/api/views.py
@@ -20,6 +20,12 @@ from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 
+from rest_framework import permissions
+
+from rest_framework import authentication
+
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 class AuthorPagination(PageNumberPagination):
     page_size = 5  # количество объектов на странице
     page_size_query_param = 'page_size'  # параметр запроса для настройки количества объектов на странице
@@ -49,6 +55,7 @@ class AuthorViewSet(ModelViewSet):
 
 
 class AuthorAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
     @csrf_exempt
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
@@ -116,10 +123,45 @@ class AuthorAPIView(APIView):
 #     def post(self, request, *args, **kwargs):
 #         return self.create(request, *args, **kwargs)
 
+
+class CustomPermission(permissions.BasePermission):
+    """
+    Пользователи могут выполнять различные действия в зависимости от их роли.
+    """
+
+    def has_permission(self, request, view):
+        # Разрешаем только GET запросы для неаутентифицированных пользователей
+        if request.method == 'GET' and not request.user.is_authenticated:
+            return True
+
+        # Разрешаем GET и POST запросы для аутентифицированных пользователей
+        if request.method in ['GET', 'POST'] and request.user.is_authenticated:
+            return True
+
+        # Разрешаем все действия для администраторов
+        if request.user.is_superuser:
+            return True
+
+        # Во всех остальных случаях возвращаем False
+        return False
+
 class AuthorGenericAPIView(GenericAPIView, RetrieveModelMixin, ListModelMixin, CreateModelMixin, UpdateModelMixin,
                            DestroyModelMixin):
     queryset = Author.objects.all()
     serializer_class = AuthorModelSerializer
+
+    # Переопределяем атрибут permission_classes для указания нашего собственного разрешения
+    permission_classes = [CustomPermission]
+
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication]
+    # в консоле написать
+    # import django
+    # import os
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+    # django.setup()
+
+    authentication_classes = [JWTAuthentication]
 
     def get(self, request, *args, **kwargs):
         if kwargs.get(self.lookup_field):
